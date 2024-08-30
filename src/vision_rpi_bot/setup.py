ba163245f578957e17886b68cd1100346d4fa5a4
eaguiraud <28522807+eaguiraud@users.noreply.github.com>
2024-08-30 16:11:45 +0200

    subscriber and publisher created and modified

--- src/vision_rpi_bot/setup.py
+++ src/vision_rpi_bot/setup.py
@@ -20,6 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        ],
-    },
+                'publisher_node = vision_rpi_bot.publisher:main',
+                'subscriber_node = vision_rpi_bot.subscriber:main',
+            ],
+                },
 )
+
+
